c40c5e4ac677f8b1f2d16a70c4ecf1f431d1a1b5
Kamil Wronka <contact@kamilwronka.com>
2025-02-07 14:04:39 +0100

    feat: added cookie caching

--- apps/auth/src/lib/auth.ts
+++ apps/auth/src/lib/auth.ts
@@ -34,6 +34,12 @@ export const auth = betterAuth({
       },
     },
   },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
   emailAndPassword: {
     enabled: true,
     minPasswordLength: 1,
