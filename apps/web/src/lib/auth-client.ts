dfdaa376ed410df512517726ee8dfb153dacca64
Kamil <kamilwronka7@gmail.com>
2025-06-02 22:30:51 +0200

    fix: idp path

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { AUTH_SERVICE_URL } from "config/auth";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: AUTH_SERVICE_URL,
+  baseURL: `${AUTH_SERVICE_URL}/idp`,
 });
