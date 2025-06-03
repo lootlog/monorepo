b3333ddc153dfb4ec837aeb1c458e7eece574f49
Kamil <kamilwronka7@gmail.com>
2025-06-03 02:45:21 +0200

    fix: auth token added

--- apps/web/src/hooks/api/use-api-client.ts
+++ apps/web/src/hooks/api/use-api-client.ts
@@ -1,8 +1,18 @@
-import { apiClient } from "@/lib/api-client";
+import { API_URL } from "config/api";
+import { useAuthToken } from "hooks/auth/use-auth-token";
+import axios from "axios";
 import { useSession } from "hooks/auth/use-session";
 
 export const useApiClient = () => {
   const { data: session } = useSession();
+  const { data: token } = useAuthToken();
 
-  return { client: apiClient, isAuthenticated: !!session };
+  const client = axios.create({
+    baseURL: API_URL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return { client: client, isAuthenticated: !!session };
 };
---
+++ apps/web/src/hooks/auth/use-auth-token.tsx
@@ -0,0 +1,30 @@
+import { AUTH_SERVICE_URL } from "config/auth";
+import { useSession } from "hooks/auth/use-session";
+import { useQuery } from "@tanstack/react-query";
+
+export const useAuthToken = () => {
+  const session = useSession();
+  const isAuthenticated = !!session.data;
+  const sessionToken = session.data?.session.token;
+
+  const query = useQuery({
+    queryKey: ["auth-token"],
+    enabled: isAuthenticated && !!sessionToken,
+    select: (data: { token: string }) => data.token,
+    queryFn: async () => {
+      const response = await fetch(`${AUTH_SERVICE_URL}/idp/token`, {
+        headers: {
+          Authorization: `Bearer ${sessionToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch auth token");
+      }
+
+      return response.json();
+    },
+  });
+
+  return query;
+};
