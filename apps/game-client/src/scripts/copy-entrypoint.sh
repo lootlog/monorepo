d3cd75c62503924d93cc7450917571c4cbd1fc3f
Kamil <kamilwronka7@gmail.com>
2025-06-03 04:22:54 +0200

    fix: game client build

--- apps/game-client/src/scripts/copy-entrypoint.sh
+++ apps/game-client/src/scripts/copy-entrypoint.sh
@@ -10,15 +10,15 @@ env=${ENVIRONMENT:-$default_env}
 echo "Using environment x: $env";
 
 # Copy the template file to the dist directory
-cp src/templates/entrypoint.js dist/entrypoint.user.js;
+cp src/templates/entrypoint.js dist/@lootlog/entrypoint.user.js;
 
 # Insert environment variable into the copied file at line 23
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' -e '23i\
   const env = "'$env'";
-  ' dist/entrypoint.user.js
+  ' dist/@lootlog/entrypoint.user.js
 else
   sed -i -e '23i\
   const env = "'$env'";
-  ' dist/entrypoint.user.js
+  ' dist/@lootlog/entrypoint.user.js
 fi
