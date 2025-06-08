02c00e283bade68900491f4f552f78b9a3455df9
Kamil <kamilwronka7@gmail.com>
2025-06-08 15:06:56 +0200

    feat: scaling and tinkering

--- apps/api/src/db/prisma.service.ts
+++ apps/api/src/db/prisma.service.ts
@@ -17,4 +17,8 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
 }
