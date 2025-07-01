fea5f51cc892623316b25b2cdcfe5f8584687ba4
Kamil Wronka <kamil.wronka@ext.allfunds.com>
2025-07-02 00:07:37 +0200

    fix: colors

--- apps/game-client/src/features/timers/components/single-timer.tsx
+++ apps/game-client/src/features/timers/components/single-timer.tsx
@@ -33,14 +33,46 @@ const NPC_NAMES: { [key: string]: { shortname: string; longname: string } } = {
 };
 
 export const COLORS = {
-  red: { bg: "ll-bg-red-500", border: "ll-border-red-500" },
-  green: { bg: "ll-bg-green-500", border: "ll-border-green-500" },
-  blue: { bg: "ll-bg-indigo-800", border: "ll-border-indigo-800" },
-  yellow: { bg: "ll-bg-yellow-500", border: "ll-border-yellow-500" },
-  purple: { bg: "ll-bg-purple-500", border: "ll-border-purple-500" },
-  orange: { bg: "ll-bg-orange-500", border: "ll-border-orange-500" },
-  pink: { bg: "ll-bg-pink-500", border: "ll-border-pink-500" },
-  white: { bg: "ll-bg-gray-400", border: "ll-border-gray-400" },
+  red: {
+    bg: "ll-bg-red-500/20 hover:ll-bg-red-500/40",
+    bgNoOpacity: "ll-bg-red-500",
+    border: "ll-border-red-500",
+  },
+  green: {
+    bg: "ll-bg-green-500/20 hover:ll-bg-green-500/40",
+    bgNoOpacity: "ll-bg-green-500",
+    border: "ll-border-green-500",
+  },
+  blue: {
+    bg: "ll-bg-indigo-800/20 hover:ll-bg-indigo-800/40",
+    bgNoOpacity: "ll-bg-indigo-800",
+    border: "ll-border-indigo-800",
+  },
+  yellow: {
+    bg: "ll-bg-yellow-500/20 hover:ll-bg-yellow-500/40",
+    bgNoOpacity: "ll-bg-yellow-500",
+    border: "ll-border-yellow-500",
+  },
+  purple: {
+    bg: "ll-bg-purple-500/20 hover:ll-bg-purple-500/40",
+    bgNoOpacity: "ll-bg-purple-500",
+    border: "ll-border-purple-500",
+  },
+  orange: {
+    bg: "ll-bg-orange-500/20 hover:ll-bg-orange-500/40",
+    bgNoOpacity: "ll-bg-orange-500",
+    border: "ll-border-orange-500",
+  },
+  pink: {
+    bg: "ll-bg-pink-500/20 hover:ll-bg-pink-500/40",
+    bgNoOpacity: "ll-bg-pink-500",
+    border: "ll-border-pink-500",
+  },
+  white: {
+    bg: "ll-bg-gray-400/20 hover:ll-bg-gray-400/40",
+    bgNoOpacity: "ll-bg-gray-400",
+    border: "ll-border-gray-400",
+  },
 };
 
 export const SingleTimer: FC<SingleTimerProps> = ({
@@ -178,7 +210,7 @@ export const SingleTimer: FC<SingleTimerProps> = ({
               key={id}
               className={cn(
                 "ll-size-3 ll-rounded-full ll-border-2 ll-box-border ll-border-transparent ll-custom-cursor-pointer",
-                color.bg,
+                color.bgNoOpacity,
                 {
                   "ll-border-solid ll-border-gray-50": selectedColor === id,
                 }
