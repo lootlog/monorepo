e378b7ee66631d1baf09133487b885023a78790c
Kamil Wronka <kamilwronka7@gmail.com>
2026-04-06 09:34:22 +0200

    hotifix: self healing debug logs

--- apps/api/src/events/services/event-kill.service.ts
+++ apps/api/src/events/services/event-kill.service.ts
@@ -462,6 +462,16 @@ export class EventKillService {
             return;
           }
 
+          if (eventHero.npcId !== npcId || eventHero.npcId === null) {
+            this.logger.debug({
+              message: "Updating hero NPC ID based on timer data",
+              guildId,
+              world,
+              newNpcId: npcId,
+              oldHeroNpcId: eventHero.npcId,
+            });
+          }
+
           const updateData = {
             ...((eventHero.npcId === null || eventHero.npcId !== npcId) && {
               npcId,
